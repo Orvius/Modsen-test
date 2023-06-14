be0cf7920e9b71576e3ece8077aa2f12878be0c9
Orvius <Okunev.Aleksander@mail.ru>
2023-06-14 23:02:51 +0300

    Change App.js and Headder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import "./css/App.css";
-import React  from "react";
+import React,{useState}  from "react";
 import Header from "./components/Header";
 
 function App() {
+  const [search, setSearch] = useState("");
+  const [category, setCategory] = useState("all");
+  const [sorting, setSorting] = useState("relevance");
+
+  const clearSearch = () => {
+    
+  }
+
   return (
    <div className="App"> 
-      <Header/>   
+      <Header
+        setSearch={setSearch}
+        setCategory={setCategory}
+        setSorting={setSorting}
+        clearSearch={clearSearch}
+      />   
    </div> 
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,34 +1,61 @@
 import React from "react";
 import "../css/Header.css";
 
-const Header = () => {
+const Header = (props) => {
     return (
         <header className="header">
         <h1 className="header__title">Search for books</h1>
         <div className="header__search">
-          <input className="search-txt" type="text" name="search" placeholder="Search..."/>
-          <button className="search-btn" type="submit">
+          <input 
+            className="search-txt" 
+            type="text" 
+            name="search" 
+            placeholder="Search..."
+            value={props.search}
+            onChange={(e) => {
+              if (e.key === "Enter") {
+                props.clearSearch();
+              }
+            }}
+          />
+          <button 
+            className="search-btn" 
+            type="submit"
+            onClick={props.clearSearch}
+          >
             <img src="../../images/loupe.png" alt="search"/>
           </button>
         </div>
         <div className="header__categories">
           <div className="select__block">
             <h2 className="select-txt">Categories</h2>
-            <select className="select-opt" name="categories">
-              <option selected value="0">all</option>
-              <option value="1">art</option>
-              <option value="2">biography</option>
-              <option value="3">computers</option>
-              <option value="4">history</option>
-              <option value="5">medical</option>
-              <option value="6">poetry</option>
+            <select 
+              className="select-opt" 
+              name="category"
+              onChange={(e) => {
+                props.setCategory(e.target.value);
+              }}
+            >
+              <option selected value="all">all</option>
+              <option value="art">art</option>
+              <option value="biography">biography</option>
+              <option value="computers">computers</option>
+              <option value="history">history</option>
+              <option value="medical">medical</option>
+              <option value="poetry">poetry</option>
             </select>
           </div>
           <div className="select__block">
             <h2 className="select-txt">Sorting by</h2>
-            <select className="select-opt" name="sorting-by">
-              <option selected value="0">relevance</option>
-              <option value="1">newest</option>
+            <select 
+              className="select-opt" 
+              name="sorting-by"
+              onChange={(e) => {
+                props.setSorting(e.target.value);
+              }}
+            >
+              <option selected value="relevance">relevance</option>
+              <option value="newest">newest</option>
             </select>
           </div>
         </div>
