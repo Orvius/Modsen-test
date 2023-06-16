f9b4000f2226ecf8a1b7604f8cf22122544b1768
Orvius <Okunev.Aleksander@mail.ru>
2023-06-16 11:14:43 +0300

    Change BookItem

--- src/components/BookItem.jsx
+++ src/components/BookItem.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import "../css/BookItem.css";
 
-const BookItem = ( {book} ) => {
+const BookItem = (props) => {
   return (
     <div className="book-block">
       <div className="book-img-container">
-        <img src={book.image} alt="book" />
+        <img src={props.book.image} alt="book" />
       </div>
-      <h2 className="book-category">{book.category}</h2>
-      {<h2 className="book-title">{book.title}</h2>}
-      <h2 className="book-author">{book.author}</h2>
+      <h2 className="book-category">{props.book.category}</h2>
+      <h2 className="book-title">{props.book.title}</h2>
+      <h2 className="book-author">{props.book.author}</h2>
     </div>
   );
 };
