a819615fa22c42ab8871c5d326f7d4abff755323
Orvius <Okunev.Aleksander@mail.ru>
2023-06-20 23:56:51 +0300

    Add .env file? connectes uuid lib

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,9 +7,10 @@ import axios from "axios";
 function App() {
   const [books, setBooks] = useState([]);
   const [count, setCount] = useState("0");
+  const [startIndex, setStartIndex] = useState("0");
 
-  const API_KEY = "AIzaSyC1IP0CY0jJ7e8HpAlXI_E-KLZUbVG3KGk";
-  const API_URL = "https://www.googleapis.com/books/v1/volumes";
+  const API_KEY = process.env.REACT_APP_API_KEY;
+  const API_URL = process.env.REACT_APP_API_URL;
 
   const searchBook = (search, category, sorting) => {
     axios
@@ -50,10 +51,51 @@ function App() {
       });
   };
 
+  const loadMore = (search, category, sorting) => {
+    setStartIndex(startIndex + 30)
+    axios
+      .get(
+        API_URL +
+        "?q=intitle:" +
+        search +
+        "+subject:" +
+        category +
+        "&startIndex=" +
+        startIndex +
+        "&maxResults=30&orderBy=" +
+        sorting +
+        "&key=" +
+        API_KEY
+      )
+      .then(function (responce) {
+        setBooks([]);
+
+        const listBooks = responce.data.items;
+
+        if (listBooks.length > 0) {
+          const setMoreBooks = listBooks.map((item) => {
+              const requestBook = {
+                category: item.volumeInfo?.categories,
+                title: item.volumeInfo.title,
+                author: item.volumeInfo.authors,
+                image: item.volumeInfo.imageLinks?.thumbnail,
+              };
+
+              return requestBook;
+            })
+
+          setBooks((books) => [...books, ...setMoreBooks]);
+        }
+      })
+      .catch((e) => {
+        console.error(e);
+      });
+  };
+
   return (
     <div className="App">
       <Header searchBook={searchBook} />
-      <Main books={books} count={count} />
+      <Main books={books} count={count} loadMore={loadMore}/>
     </div>
   );
 }
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Main.css";
 import BookItem from "../BookItem/BookItem";
+import { v4 as uuidv4 } from 'uuid';
 
 const Main = (props) => {
   return (
@@ -9,7 +10,7 @@ const Main = (props) => {
         <h2 className="main__books-count">Found {props.count} results</h2>
         <div className="main__books-block">
           {props.books?.map((surrentBook) => (
-            <BookItem key={surrentBook.key} book={surrentBook} />
+            <BookItem key={uuidv4()} book={surrentBook} />
           ))}
         </div>
         <button className="main__btn-load">Load more</button>
